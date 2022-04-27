88cd38faaad5efdd60f39af2f5158b16a179754c
unknown <flaquere@yahoo.es>
2022-04-27 09:58:10 +0200

    form working

--- backend/demo/src/main/java/com/example/demo/controllers/EventController.java
+++ backend/demo/src/main/java/com/example/demo/controllers/EventController.java
@@ -6,11 +6,10 @@ import com.example.demo.domain.service.IEventService;
 import com.example.demo.repositories.EventRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
@@ -18,12 +17,11 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/views/events")
-
 public class EventController {
     @Autowired
     private IEventService eventService;
 
-    @GetMapping("/")
+    @GetMapping("/list")
     public String eventList(Model model){
 
         List<Event> eventList = eventService.eventList();
@@ -33,9 +31,24 @@ public class EventController {
       return "/views/events/eventList";
     }
     @GetMapping("/admin/create")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     public String formCreate(Model model){
+        Event event = new Event();
+        model.addAttribute("titulo","Nuevo evento ");
+        model.addAttribute("event",event);
 
       return "/views/events/formCreate";
+    }
+
+    @PostMapping("/save")
+
+
+    public String saveEvent(@ModelAttribute Event event){
+        Date date;
+        eventService.save(event);
+        System.out.println("event saved with success");
+        return "redirect:/views/events/list";
+
     }
 
 }
